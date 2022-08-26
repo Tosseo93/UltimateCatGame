fba45d505945c0ee6f92cedc529fa752f59e81f9
Tosseo93 <d19carol@student.his.se>
2022-08-26 11:30:29 +0200

    improved enemy and player combat

--- UltimateCatGame2000/Assets/Scripts/BasicBitchEnemy.cs
+++ UltimateCatGame2000/Assets/Scripts/BasicBitchEnemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a3a16d33856f2639d2987de5cea21c2332766071b4d3a3661e594243b2bd21a
-size 9169
+oid sha256:e1d7b8778719b908811f7cff481d1113be68833affbb79baa6eecb63e77e0e6f
+size 10191
---
+++ UltimateCatGame2000/Assets/Scripts/PlayerControllers/PlayerCombatController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6dcc3df84930ce568ac68647192ff473f80ec38fe4ce9ede8474bd181c045efe
+size 811
